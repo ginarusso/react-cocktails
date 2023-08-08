c269a04faa2410e2a874b36f07fe71212d686385
Gina Russo <gpr0496@gmail.com>
2023-08-07 21:54:52 -0400

    updated css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,7 +14,7 @@ const Card = () => {
         const response = await fetch('https://the-cocktail-db3.p.rapidapi.com', {
           method: 'GET',
           headers: {
-            'X-RapidAPI-Key': '67717c6850msh99160644e11a937p125aedjsn15fa29cfa544',
+            'X-RapidAPI-Key': '',
             'X-RapidAPI-Host': 'the-cocktail-db3.p.rapidapi.com'
           }
         });
@@ -28,7 +28,7 @@ const Card = () => {
           const cocktailResponse = await fetch(`https://the-cocktail-db3.p.rapidapi.com/${id}`, {
             method: 'GET',
             headers: {
-              'X-RapidAPI-Key': '67717c6850msh99160644e11a937p125aedjsn15fa29cfa544',
+              'X-RapidAPI-Key': '',
               'X-RapidAPI-Host': 'the-cocktail-db3.p.rapidapi.com'
             }
           });
@@ -67,56 +67,3 @@ const Card = () => {
 }
 
 export default Card;
-
-
-// import React, { useState, useEffect } from "react";
-// import '../styles/styles.css';
-// import CocktailHeader from "./CocktailHeader"; 
-// import IngredientsList from "./IngredientsList";
-// import CocktailDetails from "./CocktailDetails";
-// import CocktailMethod from "./CocktailMethod";
-
-// const Card = () => {
-//   const [cocktails, setCocktails] = useState([]);
-
-//   useEffect(() => {
-//     const fetchCocktails = async () => {
-//       try {
-//         const response = await fetch('https://the-cocktail-db3.p.rapidapi.com', {
-//           method: 'GET',
-//           headers: {
-//             'X-RapidAPI-Key': '67717c6850msh99160644e11a937p125aedjsn15fa29cfa544',
-//             'X-RapidAPI-Host': 'the-cocktail-db3.p.rapidapi.com'
-//           }
-//         });
-//         const data = await response.json();
-//         setCocktails(data);
-//         console.log(data)
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-
-//     fetchCocktails();
-//   }, []);
-
-//   return (
-//     <div className="container">
-//       {cocktails.map(cocktailData => (
-//         <div className="card" key={cocktailData.id}>
-//           <CocktailHeader title={cocktailData.title} image={cocktailData.image} id={cocktailData.id} /> 
-//           <IngredientsList ingredients={cocktailData.ingredients} /> 
-//           <CocktailDetails
-//             difficulty={cocktailData.difficulty}
-//             portion={cocktailData.portion}
-//             time={cocktailData.time}
-//             description={cocktailData.description}
-//           /> 
-//           <CocktailMethod method={cocktailData.method || []} />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default Card;
